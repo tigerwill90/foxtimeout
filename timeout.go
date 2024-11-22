433d17f961d49e8d65eaf97d5090b62e0de88b6b
tigerwill90 <sylvainmuller@protonmail.com>
2024-11-22 02:32:55 +0100

    feat: update fox to v0.18.0

--- timeout.go
+++ timeout.go
@@ -20,11 +20,13 @@ import (
 	"time"
 )
 
-var bufp = sync.Pool{
-	New: func() any {
-		return bytes.NewBuffer(nil)
-	},
-}
+var (
+	bufp = sync.Pool{
+		New: func() any {
+			return bytes.NewBuffer(nil)
+		},
+	}
+)
 
 // Timeout is a middleware that ensure HTTP handlers don't exceed the configured timeout duration.
 type Timeout struct {
@@ -32,13 +34,13 @@ type Timeout struct {
 	dt  time.Duration
 }
 
-// Middleware returns a fox.MiddlewareFunc with a specified timeout and options.
+// Middleware returns a [fox.MiddlewareFunc] with a specified timeout and options.
 // This middleware function, when used, will ensure HTTP handlers don't exceed the given timeout duration.
 func Middleware(dt time.Duration, opts ...Option) fox.MiddlewareFunc {
 	return New(dt, opts...).Timeout
 }
 
-// New creates and initializes a new Timeout middleware with the given timeout duration
+// New creates and initializes a new [Timeout] middleware with the given timeout duration
 // and optional settings.3
 func New(dt time.Duration, opts ...Option) *Timeout {
 	cfg := defaultConfig()
@@ -57,13 +59,13 @@ func New(dt time.Duration, opts ...Option) *Timeout {
 	}
 }
 
-// Timeout returns a fox.HandlerFunc that runs next with the given time limit.
+// Timeout returns a [fox.HandlerFunc] that runs next with the given time limit.
 //
 // The new handler calls next to handle each request, but if a call runs for longer than its time limit,
 // the handler responds with a 503 Service Unavailable error and the given message in its body (if a custom response
-// handler is not configured). After such a timeout, writes by next to its ResponseWriter will return http.ErrHandlerTimeout.
+// handler is not configured). After such a timeout, writes by next to its ResponseWriter will return [http.ErrHandlerTimeout].
 //
-// Timeout supports the http.Pusher interface but does not support the http.Hijacker or http.Flusher interfaces.
+// Timeout supports the [http.Pusher] interface but does not support the [http.Hijacker] or [http.Flusher] interfaces.
 func (t *Timeout) Timeout(next fox.HandlerFunc) fox.HandlerFunc {
 	if t.dt <= 0 {
 		return func(c fox.Context) {
